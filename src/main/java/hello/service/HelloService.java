4867de97be75b762ae252ecdbaf34cea41dd156e
zyk <yjjc1996@163.com>
2019-09-12 00:01:48 +0800

    添加了一个service

---
+++ src/main/java/hello/service/HelloService.java
@@ -0,0 +1,10 @@
+package hello.service;
+
+/**
+ * created by 不器 on 2019/9/12.
+ */
+public class HelloService {
+    public static void main(String[] args) {
+        System.out.println("Hello Controller");
+    }
+}
